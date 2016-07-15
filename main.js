f3c2f8a206d488f1124bb5381ab1f720d508c48a
Zach Puller <zachpuller@gmail.com>
2016-07-15 21:23:42 +0200

    basic page with canvas

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('hello')
+var canvas = document.getElementById("canvas");
+var tools = document.getElementById("tools");
+var ctx
+
+ctx = canvas.getContext("2d");
+var image_data = ctx.createImageData(canvas.width,canvas.height);
+var data  = image_data.data;
+
+for (var i = 0; i < data.length;)
+{
+  data[i] = 0;
+  ++i;
+  data[i] = 0;
+  ++i;
+  data[i] = 255 * i / data.length;
+  ++i;
+  data[i] = 255;
+  ++i;
+}
+
+ctx.putImageData(image_data, 0, 0);
+
+ctx = tools.getContext("2d");
+ctx.fillStyle = "rgb(0, 200,0)";
+ctx.fillRect (0, 0, tools.width, tools.height);
