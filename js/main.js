f18f478ca7d2b68ebab99d18fbffffb92c06af59
Zach Puller <zachpuller@gmail.com>
2016-07-16 20:36:00 +0200

    disable scrolling on ipad

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ window.addEventListener('touchend', handle_mouseup);
 canvas.addEventListener('mousedown', handle_mousedown); 
 window.addEventListener('mouseup', handle_mouseup); 
 window.addEventListener('keydown', handle_keypress); 
+document.addEventListener('touchmove',function(event){ event.preventDefault(); }, false);
 
 draw_canvas_background();
 draw_help();
