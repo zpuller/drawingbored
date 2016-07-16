a1c7b98f5c88efad047a99c0d0c1df289c482f7d
Zach Puller <zachpuller@gmail.com>
2016-07-16 20:30:44 +0200

    fix ipad bug

--- js/utils.js
+++ js/utils.js
@@ -2,8 +2,11 @@
 
 function update_pos(event, obj)
 {
-  var ipadX = event.targetTouches[0].pageX;
-  var ipadY = event.targetTouches[0].pageY;
-  obj.x = Math.floor(canvas.width * ((event.offsetX || ipadX ) / canvas.clientWidth));
+  if (event.targetTouches)
+  {
+    var ipadX = event.targetTouches[0].pageX;
+    var ipadY = event.targetTouches[0].pageY;
+  }
+  obj.x = Math.floor(canvas.width * ((event.offsetX || ipadX) / canvas.clientWidth));
   obj.y = Math.floor(canvas.height * ((event.offsetY || ipadY) / canvas.clientHeight));
 }
